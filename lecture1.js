69c1277fb4162b1f56fb89bfe01e17a01d6388ad
Margaret Orem <MargaretAoko@users.noreply.github.com>
2016-10-25 21:55:32 +0000

    Update lecture1.js

--- lecture1.js
+++ lecture1.js
@@ -2,28 +2,32 @@
  * 1. Are the following variable valid? If it is not please explain it and correct them using the est practise
  */
 
-var 1stCar = 'Doge';
-var var = 'variable';
-var first name = 'Jerry';
-var Sch00lName = 'MEST';
-var totalnumber = 100;
+var 1stCar = 'Doge'; //Javascript uses camelcase for identifying names of variables and function the identifier name here is valid cause it's camelCase.
+var var = 'variable';//The variable tag var has been assigned twice making the code invalid. It should be only one. var = 'Variable';
+var first name = 'Jerry';//JavaScript uses camelCase for identifying names of variables and functions. Var firstName = 'Jerry';
+var Sch00lName = 'MEST';//The variable name Sch00LName has digit '0' instade of letter 'o' then it's invalid because it's not camelCase it starts with caps 'S'. var schoolName = 'Jerry';
+var totalnumber = 100;//variable name is not camelCase. var totalNumber = 100;
 
 /*
  * 2. Are the following variable valid? If it is not please explain it and correct them by using the best practise
  */
-var greeting  =  'It's nice to me you today!';
-var response = "Ashwin said, "Yes Sir!"";
-
+var greeting  =  'It's nice to me you today!'; // When assigning variables in javascript you use single '' or double "" in this variable, javascrpt will detect the first single quotes of the string and only excute that
+var response = "Ashwin said, "Yes Sir!"";//As much as you may want to consider grammar javascripts detects the how you use your quotes to when writing strings. For this case it will only excute "Ashwin," because the string is closed there. In order to excute the whole string this how to write it. var response = "Ashwin said, Yes Sir!";
 
 /*
- * 3. Try 0.3 + 0.1 === 0.4, explain what happen here
+ * 3. Try 0.3 + 0.1 === 0.4, explain what happen here // Checks if both side are equale to each other.
  */
 
 
 /*
  * 4. Build a pizza object by using all three object creation method, it must has at least 4 properties
  */
-
+function () pizza = { 
+  type: " Vegetable" 
+  price: " $15 " 
+  size: "Small" 
+  shape: "Circular", 
+  pizzaType : function() { return this.type + " " + this.size; } } 
 /*
  *  5. Build a Animal object by using object constructor function, and add at least two method to prototype,
  *     create two animals from it.
@@ -36,7 +40,11 @@ var response = "Ashwin said, "Yes Sir!"";
 /*
  * 7. var studentScores = [50, 67, 80, 90, 100, 30, 88, 68]; write a program by using studentScores array ,for loop and conditions.
  */
+ var studentScores = [50, 67, 80, 90, 100, 30, 88, 68];
+ for (var i = 0; i < studentScore; i++1) { 
+   console.log(studentScore[i]); } 
 
 /*
  * 8. var studentScores = [50, 67, 80, 90, 100, 30, 88, 68]; write a program by using studentScores array , array.forEach and conditions.
  */
+ var studentScores = [50, 67, 80, 90, 100, 30, 88, 68]; 
